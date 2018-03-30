4d66d8cd449e8322487597a97c2424353ec46d84
Jonathan Tung <jtung23@gmail.com>
2018-03-30 11:06:46 -0700

    initial server inital django setup

---
+++ databasemodel.py
@@ -0,0 +1,98 @@
+{
+	'sessionID': 1,
+	'userID': 01,
+	'date': '03/27/2018',
+	'title': 'Brisket #1',
+	'information': {
+		'animal': 'cow',
+		'grade': 'USDA prime',
+		'weight': 16.1,
+		'after_trim_weight': 11.1,
+		'notes': 'lorem ipsum',
+		'description_notes': 'fatty point, solid thickness',
+		'interval': 30
+	},
+	'record': {
+		1: {
+			'time': 600,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		2: {
+			'time': 700,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		3: {
+			'time': 800,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		4: {
+			'time': 900,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		5: {
+			'time': 1000,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		6: {
+			'time': 1100,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		7: {
+			'time': 1200,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		8: {
+			'time': 1300,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		9: {
+			'time': 1400,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+		10: {
+			'time': 1500,
+			'internal_temp': 120,
+			'grill_temp': 250,
+			'vent_1': '50%',
+			'vent_2': '50%',
+			'vent_3': '50%'
+		},
+	}
+
+}
