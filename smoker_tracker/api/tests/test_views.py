24abc76d0544cd4d0b7c90abccadedb6b341c11c
Jonathan Tung <jtung23@gmail.com>
2018-06-07 14:25:28 -0700

    simplified model to 4 fields, modified ModelSerializer, adding urls to urls.py in api dir. Approaching setting up REST using tests

---
+++ smoker_tracker/api/tests/test_views.py
@@ -0,0 +1,9 @@
+import json
+from rest_framework import status
+from django.test import TestCase, Client
+from django.urls import reverse
+from ..models import SmokeSession
+from ..serializers import SessionSerializer
+
+# initializes the apiclient app
+client = Client()
