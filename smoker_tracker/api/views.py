22ff0b13bec5c12c00b6c04b6283530875dde6e0
Jonathan Tung <jtung23@gmail.com>
2018-06-07 12:09:23 -0700

    redoing api backend

--- smoker_tracker/api/views.py
+++ smoker_tracker/api/views.py
@@ -1,9 +1,37 @@
-from django.shortcuts import render
-from api.models import SmokeSession
-from api.serializers import SessionSerializer
-from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import SmokeSession
+from .serializers import SessionSerializer
+
 
 # Create your views here.
-class SessionListCreate(generics.ListCreateAPIView):
-    queryset = SmokeSession.objects.all()
-    serializer_class = SessionSerializer
+
+@api_view(['GET','POST','PUT'])
+def get_delete_update_session(request, pk):
+    try:
+        session = SmokeSession.objects.get(pk=pk)
+    except SmokeSession.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method="GET":
+        return Response({})
+    elif request.method="POST"":
+        return Response({})
+    elif request.method="PUT":
+        return Response({})
+
+@api_view(['GET', 'POST'])
+def get_post_session(request):
+    # get all puppies
+    if request.method == 'GET':
+        return Response({})
+    # insert a new record for a puppy
+    elif request.method == 'POST':
+        return Response({})
+
+
+
+# class SessionListCreate(generics.ListCreateAPIView):
+#     queryset = SmokeSession.objects.all()
+#     serializer_class = SessionSerializer
