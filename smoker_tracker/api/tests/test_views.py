c107208e490b6a1772af943e8e2685282db96b6e
Jonathan Tung <jtung23@gmail.com>
2018-06-08 02:35:48 -0700

    set up test cases but urls.py in api and the views.py in api do not work. Have to figure out pk usage before proceeding. Consider starting over and modifying test cases with all actual possible fields such as the JSON arrayfield for columns

--- smoker_tracker/api/tests/test_views.py
+++ smoker_tracker/api/tests/test_views.py
@@ -7,3 +7,81 @@ from ..serializers import SessionSerializer
 
 # initializes the apiclient app
 client = Client()
+
+class GetAllSessionsTest(TestCase):
+
+    def setUp(self):
+        SmokeSession.objects.create(
+            sessionId=1,
+            userId=1,
+            title="hello",
+            smoker="WSM"
+        )
+        SmokeSession.objects.create(
+            sessionId=2,
+            userId=1,
+            title="bye",
+            smoker="WSM"
+        )
+        SmokeSession.objects.create(
+            sessionId=1,
+            userId=2,
+            title="yes",
+            smoker="WSM"
+        )
+        SmokeSession.objects.create(
+            sessionId=1,
+            userId=2,
+            title="no",
+            smoker="WSM"
+        )
+
+    def test_get_all_sessions(self):
+        response = client.get(reverse('get_post_session'))
+        sessions = SmokeSession.objects.all()
+        serializer = SessionSerializer(sessions, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetSingleSessionTest(TestCase):
+
+    def setUp(self):
+        self.hello = SmokeSession.objects.create(
+            sessionId=1,
+            userId=1,
+            title="hello",
+            smoker="WSM"
+        )
+        self.bye = SmokeSession.objects.create(
+            sessionId=2,
+            userId=1,
+            title="bye",
+            smoker="WSM"
+        )
+        self.yes = SmokeSession.objects.create(
+            sessionId=1,
+            userId=2,
+            title="yes",
+            smoker="WSM"
+        )
+        self.no = SmokeSession.objects.create(
+            sessionId=1,
+            userId=2,
+            title="no",
+            smoker="WSM"
+        )
+
+        
+    def test_get_valid_single_session(self):
+        response = client.get(
+            reverse('get_delete_update_session',
+                kwargs={'pk': self.hello.pk}))
+        session = SmokeSession.objects.get(pk=self.hello.pk)
+        serializer = SessionSerializer(session)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+    def test_get_invalid_single_session(self):
+        response = client.get(
+            reverse('get_delete_update_session', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
--- smoker_tracker/api/views.py
+++ smoker_tracker/api/views.py
@@ -1,6 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from .models import SmokeSession
 from .serializers import SessionSerializer
 
@@ -8,24 +8,28 @@ from .serializers import SessionSerializer
 # Create your views here.
 
 @api_view(['GET','POST','PUT'])
+@permission_classes((permissions.AllowAny,))
 def get_delete_update_session(request, pk):
     try:
         session = SmokeSession.objects.get(pk=pk)
     except SmokeSession.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
     
-    if request.method="GET":
+    if request.method=="GET":
         return Response({})
-    elif request.method="POST"":
+    elif request.method=="POST":
         return Response({})
-    elif request.method="PUT":
+    elif request.method=="PUT":
         return Response({})
 
 @api_view(['GET', 'POST'])
+@permission_classes((permissions.AllowAny,))
 def get_post_session(request):
     # get all puppies
     if request.method == 'GET':
-        return Response({})
+        sessions = SmokeSession.objects.all()
+        serializer = SessionSerializer(sessions, many=True)
+        return Response(serializer.data)
     # insert a new record for a puppy
     elif request.method == 'POST':
         return Response({})
